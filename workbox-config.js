756c15e88f4ad2fa16353f16157637e8cb7c634b
Shobhit1458 <ahuja.shobhit14@gmail.com>
2023-08-20 20:24:55 +0530

    commit-5

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,5 @@
 module.exports = {
  
- plugins: [
-  {
-   name: 'preset-default',
-  },
-   'cleanupIDs'
-  ]
   globDirectory: 'docs',
   globPatterns: [
     '**/*.{html,js,css,png,svg,jpg,gif,json,ttf,woff,woff2,eot,ico,webmanifest,map}'
@@ -13,4 +7,10 @@ module.exports = {
   swDest: 'docs/service-worker.js',
   clientsClaim: true,
   skipWaiting: true
+  plugins: [
+   {
+    name: 'preset-default',
+   },
+    'cleanupIDs'
+   ]
 };
